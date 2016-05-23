3ac38216a358c709c4cb381ad82a9a2e8de812f1
Reid Blomquist <reid@theprogrampdx.com>
2016-05-22 17:52:07 -0700

    clean up tut/boilerplate stuff

--- main.go
+++ main.go
@@ -22,10 +22,6 @@ func main() {
 		AccessControlMaxAge:           3600,
 	})
 	router, err := rest.MakeRouter(
-		rest.Get("/countries", GetAllCountries),
-		rest.Post("/countries", PostCountry),
-		rest.Get("/countries/:code", GetCountry),
-		rest.Delete("/countries/:code", DeleteCountry),
 		rest.Get("/rgba", GetRgba),
 		rest.Post("/rgba", PostRgba),
 	)
@@ -36,79 +32,11 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
 
-type Country struct {
-	Code string
-	Name string
-}
-
 type Rgba struct {
-	R float32;
-	G float32;
-	B float32;
-	A float32;
-}
-
-var countryStore = map[string]*Country{}
-
-var countryLock = sync.RWMutex{}
-
-func GetCountry(w rest.ResponseWriter, r *rest.Request) {
-	code := r.PathParam("code")
-
-	countryLock.RLock()
-	var country *Country
-	if countryStore[code] != nil {
-		country = &Country{}
-		*country = *countryStore[code]
-	}
-	countryLock.RUnlock()
-
-	if country == nil {
-		rest.NotFound(w, r)
-		return
-	}
-	w.WriteJson(country)
-}
-
-func GetAllCountries(w rest.ResponseWriter, r *rest.Request) {
-	countryLock.RLock()
-	countries := make([]Country, len(countryStore))
-	i := 0
-	for _, country := range countryStore {
-		countries[i] = *country
-		i++
-	}
-	countryLock.RUnlock()
-	w.WriteJson(&countries)
-}
-
-func PostCountry(w rest.ResponseWriter, r *rest.Request) {
-	country := Country{}
-	err := r.DecodeJsonPayload(&country)
-	if err != nil {
-		rest.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if country.Code == "" {
-		rest.Error(w, "country code required", 400)
-		return
-	}
-	if country.Name == "" {
-		rest.Error(w, "country name required", 400)
-		return
-	}
-	countryLock.Lock()
-	countryStore[country.Code] = &country
-	countryLock.Unlock()
-	w.WriteJson(&country)
-}
-
-func DeleteCountry(w rest.ResponseWriter, r *rest.Request) {
-	code := r.PathParam("code")
-	countryLock.Lock()
-	delete(countryStore, code)
-	countryLock.Unlock()
-	w.WriteHeader(http.StatusOK)
+	R float32
+	G float32
+	B float32
+	A float32
 }
 
 var rgbaStore = map[int]*Rgba{}
